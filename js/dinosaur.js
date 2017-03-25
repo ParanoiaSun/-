f74d051ce0f59167b81c32ff750e2601fa656da0
151250127 <151250127@smail.nju.edu.cn>
2017-03-25 14:59:47 +0800

    A running dinosaur

--- js/dinosaur.js
+++ js/dinosaur.js
@@ -1,8 +1,47 @@
 var dinosaurObj = function () {
 	this.x;
 	this.y;
+	this.width;
+	this.height;
+	this.state;
+	this.count;
+	this.dinosaurBody = new Image();
+	this.dinosaurEye = new Image();
+	this.dinosaurLeg = [];
+	this.scale;
+	this.dinosaurTimer;
+	this.dinosaurCount;
 }
 
 dinosaurObj.prototype.init = function (){
+	this.x = canWidth * 0.5;
+	this.y = canHeight * 0.5;
+	this.state = 0;
+	this.count = 0;
+	this.dinosaurBody.src = "./src/dinosaurBody.png";
+	this.dinosaurEye.src = "./src/dinosaurEye0.png";
+	for(var i = 0; i < 3; i++){
+		this.dinosaurLeg[i] = new Image();
+		this.dinosaurLeg[i].src = "./src/dinosaurLeg" + i +".png"
 
+	}
+	this.scale = 0;
+	this.dinosaurTimer = 0;
+	this.dinosaurCount = 0;
+}
+
+dinosaurObj.prototype.draw = function () {
+	ctx.save();
+
+	this.dinosaurTimer += deltaTime;
+	if(this.dinosaurTimer > 100){
+		this.dinosaurCount = (this.dinosaurCount + 1) % 2;
+		this.dinosaurTimer %= 100;
+	}
+
+	var count = this.dinosaurCount + 1;
+	ctx.drawImage(this.dinosaurBody, this.x - 39, this.y - 33, 78, 66);
+	ctx.drawImage(this.dinosaurEye, this.x - 39 + 47, this.y - 33 + 5, 4, 4);
+	ctx.drawImage(this.dinosaurLeg[count], this.x - 39 + 16, this.y - 33 + 66, 38, 18);
+	ctx.restore();
 }
--- js/main.js
+++ js/main.js
@@ -15,7 +15,9 @@ var spd;
 document.body.onload = game;
 
 function game(){
-
+	init();
+	lastTime = Date.now();
+	gameloop();
 }
 
 function init(){
@@ -24,4 +26,31 @@ function init(){
 	dinosaur = new dinosaurObj();
 	ground = new groundObj();
 	monster = new monsterObj();
+
+	can = document.getElementById("canvas");
+	ctx = can.getContext('2d');
+
+	canHeight = can.height;
+	canWidth = can.width;
+
+	dinosaur.init();
+}
+
+function gameloop(){
+	//setInterval, setTimeout
+
+	//当前绘制完成之后，去根据机器的性能来确定间隔多长时间来绘制下一帧
+	//智能计算
+	// frame per second FPS, 会导致帧之间的间隔不固定
+	requestAnimFrame(gameloop);
+	//返回从1970.1.1到现在的毫秒数
+	var now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;
+	if(deltaTime > 40)
+		deltaTime = 40;
+	//console.log(deltaTime);这里暂停将鼠标滚轮往上就ok哦
+	//利用时间差来调整运动速度
+	ctx.clearRect(0, 0, canWidth, canHeight);
+	dinosaur.draw();
 }
